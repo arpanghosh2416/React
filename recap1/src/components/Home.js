b4b79446cf1e9c307284b99c4a8e0406637a66ca
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-02-16 23:39:28 +0530

    supratim531: Update home.js in recap1

--- recap1/src/components/Home.js
+++ recap1/src/components/Home.js
@@ -1,9 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Home() {
+  const [data, setData] = useState({ username: 'sayan', password: 'sayan123', email: 'sayan@gmail.com' });
+
   return (
     <div>
       <h1>This is Home page</h1>
+      <hr />
+      <div className="" style={{ margin: "10px 0px" }}>
+        <form style={{ display: "flex", gap: "10px", flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+          <input type="text" value={data.username} onChange={e => setData({ ...data, username: e.target.value })} />
+          <input type="password" value={data.password} onChange={e => setData({ ...data, password: e.target.value })} />
+          <input type="email" value={data.email} onChange={e => setData({ ...data, email: e.target.value })} />
+          <button>Submit</button>
+        </form>
+      </div>
+      <div className="" style={{ fontSize: "20px" }}>
+        <b>{`{username: ${data.username}, password: ${data.password}, email: ${data.email}}`}</b>
+      </div>
     </div>
   );
 }
