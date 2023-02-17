b38ee2cab63c539979420b877ad4e4187d89400c
supratim531 <91423964+supratim531@users.noreply.github.com>
2023-02-18 01:41:53 +0530

    supratim531: Tested flushSync and startTransition of react-v18.0.2 onwards...

--- recap1/src/components/About.js
+++ recap1/src/components/About.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { startTransition, useState } from 'react';
+import { flushSync } from 'react-dom';
 
 export default function About() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [phone, setPhone] = useState('');
+
+  console.log("About Component rendered:", { name, email, phone });
+
   return (
     <div>
       <h1>This is About page</h1>
+      <hr />
+      <div className="" style={{ fontSize: "20px", fontWeight: "700" }}>
+        {`{name: ${name}, email: ${email}, phone: ${phone}}`}
+      </div>
+      <button onClick={
+        () => {
+          // setName("sayan");
+          // setEmail("sayan@gmail.com");
+          // setPhone("7596841203");
+
+          // flushSync(() => {
+          //   setName('sayan');
+          // });
+          // flushSync(() => {
+          //   setEmail("sayan@gmail.com");
+          // });
+          // flushSync(() => {
+          //   setPhone("7596841203");
+          // });
+
+          setEmail("sayan@gmail.com");
+          startTransition(() => {
+            setName('sayan');
+            setPhone("7596841203");
+          });
+        }
+      }>Update Data</button>
     </div>
   );
 }
